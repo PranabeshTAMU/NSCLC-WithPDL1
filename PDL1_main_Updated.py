a5bae742c64c7674dda878e11eb0b319d49d10d8
PranabeshTAMU <144166072+PranabeshTAMU@users.noreply.github.com>
2024-07-13 15:35:53 -0500

    Update PDL1_main_Updated.py

--- PDL1_main_Updated.py
+++ PDL1_main_Updated.py
@@ -1,4 +1,4 @@
-import numpy as np
+mport numpy as np
 import pandas as pd
 import itertools
 from itertools import combinations
@@ -7,15 +7,14 @@ from NSCLC_PDL1_two_faults import NSCLC_PDL1_two_faults
 from NSCLC_PDL1_three_faults import NSCLC_PDL1_three_faults
 #import matplotlib.pyplot as plt
 
-drugs = ['Osimertinib', 'Selpercatinib', 'Trastuzumab_deruxtecan', 'Savolitinib', 'Dabrafenib_Trametinib', 'Durvalumab', 'Alpelisib', 'Enzastaurin', 'Lumakras', 'RG7388', 'Ribociclib', 'STAT_Inhibitor', 'Everolimus', 'Lorlatinib']
+drugs = ['Osimertinib', 'Selpercatinib', 'Trastuzumab_deruxtecan', 'Savolitinib', 'Dabrafenib', 'Durvalumab', 'Trametinib', 'Alpelisib', 'Enzastaurin', 'Lumakras', 'RG7388', 'Ribociclib', 'STAT_Inhibitor', 'Everolimus', 'Lorlatinib', 'MK2206']
 
-#num_drugs = int(input("Enter the number of drugs in each combination (0-13): "))
-num_drugs = n = 1 #number of drugs
+num_drugs = n = 4 #number of drugs
 
-A = np.zeros((49, 16384))
+A = np.zeros((43, 65536))
 
 # "i" iterates over faults
-for i in range (49): # goes from fault 0 to 41
+for i in range (43): # goes from fault 0 to 41
     for d1 in range(2):
         for d2 in range(2):
             for d3 in range(2):
@@ -30,9 +29,11 @@ for i in range (49): # goes from fault 0 to 41
                                                 for d12 in range(2):
                                                     for d13 in range(2):
                                                         for d14 in range(2):
-                                                            m = 8192 * d1 + 4096 * d2 + 2048 * d3 + 1024 * d4 + 512 * d5 + 256 * d6 + 128 * d7 + 64 * d8 + 32 * d9 + 16 * d10 + 8 * d11 + 4 * d12 + 2 * d13 + d14 + 1
-                                                            if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 <= n:
-                                                                A[i,m] = NSCLC_PDL1_one_fault(i, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14)
+                                                            for d15 in range(2):
+                                                                for d16 in range(2):
+                                                                   m = 32768 * d1 + 16384 * d2 + 8192 * d3 + 4096 * d4 + 2048 *  d5 + 1024 * d6 + 512 * d7 + 256 *  d8 + 128 * d9 + 64 * d10 + 32 * d11 + 16 *  d12 + 8 *d13 + 4 *  d14 + 2 *d15 + d16+ 1
+                                                                   if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 + d15 + d16<= n:
+                                                                       A[i,m] = NSCLC_PDL1_one_fault(i, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16)
                                                             
 
 A = np.sum(A, axis=0)  # Summing the measures across all faults
@@ -44,16 +45,14 @@ A = A.reshape(-1,1) # Calculating Transpose of that matrix
 
 # Overall measure for two fault network
 
-#B = np.zeros((38, 38, 8193))
-
-B = np.zeros((49, 49, 16384))
+B = np.zeros((43, 43, 65536))
 
 # Generate all possible combinations of drugs
 #drug_combinations = list(itertools.product([0, 1], repeat=12))
 
 # "i" and "j" iterate over faults
-for i in range(49):
-    for j in range(49):
+for i in range(43):
+    for j in range(43):
         for d1 in range(2):
             for d2 in range(2):
                 for d3 in range(2):
@@ -68,15 +67,12 @@ for i in range(49):
                                                     for d12 in range(2):
                                                         for d13 in range(2):
                                                             for d14 in range(2):
-                                                                m = 8192 * d1 + 4096 * d2 + 2048 * d3 + 1024 * d4 + 512 * d5 + 256 * d6 + 128 * d7 + 64 * d8 + 32 * d9 + 16 * d10 + 8 * d11 + 4 * d12 + 2 * d13 + d14 + 1
-                                                                if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 <= n:
-                                                                    B[i,j,m] = NSCLC_PDL1_two_faults(i, j, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14)
-                                                            
+                                                                for d15 in range(2):
+                                                                    for d16 in range(2):
+                                                                        m = 32768 * d1 + 16384 * d2 + 8192 * d3 + 4096 * d4 + 2048 *  d5 + 1024 * d6 + 512 * d7 + 256 *  d8 + 128 * d9 + 64 * d10 + 32 * d11 + 16 *  d12 + 8 *d13 + 4 *  d14 + 2 *d15 + d16+ 1
+                                                                        if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 + d15+ d16<= n:                                                        
+                                                                            B[i, j, m] = NSCLC_PDL1_two_faults(i, j, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16)
 
-#B = np.sum(np.sum(B, axis=0), axis=0)  # Summing the measures across all faults
-#B = B[:, np.any(B, axis=1)]  # Removing the zero columns
-#B = B / np.max(np.abs(B))  # Normalizing the measure
-#B = np.transpose(B)
 
 B = np.sum(np.sum(B, axis=0), axis=0)  # Summing the measures across all faults
 mask = (B !=0)
@@ -85,22 +81,14 @@ B = B / np.max(B) # Normalising the measure
 B = B.reshape(-1,1) # Calculating Transpose of that matrix
 
 
-'''
-B = B[np.newaxis, :]  # Adding a new axis to make it 
-B = B[:, np.any(B, axis=0)]  # Removing the zero columns 
-B = B / np.max(np.abs(B))  # Normalizing the measure
-B = np.transpose(B)  # Transposing to 
-'''
-#C = np.zeros((38, 38, 38, 8193))
-
-C = np.zeros((49, 49, 49, 16384))
+C = np.zeros((43, 43, 43, 65536))
 # Generate all possible combinations of drugs
 #drug_combinations = list(itertools.product([0, 1], repeat=12))
 
 # "i", "j", "k" iterate over faults
-for i in range(49):
-    for j in range(49):
-        for k in range(49):
+for i in range(43):
+    for j in range(43):
+        for k in range(43):
             for d1 in range(2):
                 for d2 in range(2):
                     for d3 in range(2):
@@ -115,10 +103,11 @@ for i in range(49):
                                                         for d12 in range(2):
                                                             for d13 in range(2):
                                                                 for d14 in range(2):
-                                                                    m = 8192 * d1 + 4096 * d2 + 2048 * d3 + 1024 * d4 + 512 * d5 + 256 * d6 + 128 * d7 + 64 * d8 + 32 * d9 + 16 * d10 + 8 * d11 + 4 * d12 + 2 * d13 + d14 + 1
-                                                                    if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 <= n:
-                                                                        C[i,j,k,m] = NSCLC_PDL1_three_faults(i, j, k, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14)
-                                                            
+                                                                    for d15 in range(2):
+                                                                        for d16 in range(2):
+                                                                            m = 32768 * d1 + 16384 * d2 + 8192 * d3 + 4096 * d4 + 2048 *  d5 + 1024 * d6 + 512 * d7 + 256 *  d8 + 128 * d9 + 64 * d10 + 32 * d11 + 16 *  d12 + 8 *d13 + 4 *  d14 + 2 *d15 + d16+ 1
+                                                                            if d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 + d15+ d16<= n:                                                            
+                                                                                C[i,j,k,m] = NSCLC_PDL1_three_faults(i, j, k, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16)
 
 C = np.sum(np.sum(np.sum(C, axis=0), axis=0), axis=0)  # Summing the measures across different faults 
 mask = (C !=0)
@@ -126,16 +115,8 @@ C = C[mask] # Removing the zero columns
 C = C / np.max(C) # Normalising the measure
 C = C.reshape(-1,1) # Calculating Transpose of that matrix
 
-'''
-C = C[np.newaxis, :]  # Adding a new axis to make it 
-C = C[:, np.any(C, axis=0)]  # Removing the zero columns 
-C = C / np.max(np.abs(C))  # Normalizing the measure
-C = np.transpose(C)  # Transposing 
-'''
-
-
-if num_drugs < 0 or num_drugs > 14:
-    print("Invalid input. Please enter a number between 0 and 14.")
+if num_drugs < 0 or num_drugs > 16:
+    print("Invalid input. Please enter a number between 0 and 15.")
 else:
     # Define the drug names
 # Initialize the output list with 'Untreated'
@@ -156,42 +137,47 @@ else:
                                                 for d12 in range(2):
                                                     for d13 in range(2):
                                                         for d14 in range(2):
+                                                            for d15 in range(2):
+                                                                for d16 in range(2):
                                                         # Create an empty combination
-                                                                combination = []
+                                                                    combination = []
 
                                                         # Append individual drugs if the corresponding variable is 1
-                                                                if d1:
-                                                                    combination.append(drugs[0])
-                                                                if d2:
-                                                                    combination.append(drugs[1])
-                                                                if d3:
-                                                                    combination.append(drugs[2])
-                                                                if d4:
-                                                                    combination.append(drugs[3])
-                                                                if d5:
-                                                                    combination.append(drugs[4])
-                                                                if d6:
-                                                                    combination.append(drugs[5])
-                                                                if d7:
-                                                                    combination.append(drugs[6])
-                                                                if d8:
-                                                                    combination.append(drugs[7])
-                                                                if d9:
-                                                                    combination.append(drugs[8])
-                                                                if d10:
-                                                                    combination.append(drugs[9])
-                                                                if d11:
-                                                                    combination.append(drugs[10])
-                                                                if d12:
-                                                                    combination.append(drugs[11])
-                                                                if d13:
-                                                                    combination.append(drugs[12])
-                                                                if d14:
-                                                                    combination.append(drugs[13])                                                                     
-                                                                                      
+                                                                    if d1:
+                                                                        combination.append(drugs[0])
+                                                                    if d2:
+                                                                        combination.append(drugs[1])
+                                                                    if d3:
+                                                                        combination.append(drugs[2])
+                                                                    if d4:
+                                                                        combination.append(drugs[3])
+                                                                    if d5:
+                                                                        combination.append(drugs[4])
+                                                                    if d6:
+                                                                        combination.append(drugs[5])
+                                                                    if d7:
+                                                                        combination.append(drugs[6])
+                                                                    if d8:
+                                                                        combination.append(drugs[7])
+                                                                    if d9:
+                                                                        combination.append(drugs[8])
+                                                                    if d10:
+                                                                        combination.append(drugs[9])
+                                                                    if d11:
+                                                                        combination.append(drugs[10])
+                                                                    if d12:
+                                                                        combination.append(drugs[11])
+                                                                    if d13:
+                                                                        combination.append(drugs[12])
+                                                                    if d14:
+                                                                        combination.append(drugs[13])                                                                     
+                                                                    if d15:
+                                                                        combination.append(drugs[14])
+                                                                    if d16:
+                                                                        combination.append(drugs[15])                                                                                         
                                                         # Add the combination to the output list with formatting
-                                                                if combination and len(combination) <= n:
-                                                                    output_list.append("{}".format(' + '.join(combination)))
+                                                                    if combination and len(combination) <= n:
+                                                                        output_list.append("{}".format(' + '.join(combination)))
     combo_array = np.array(output_list)
 # Print the output list
     #for combo in output_list:
@@ -199,41 +185,4 @@ else:
     #output_one_fault = np.column_stack((combo_array, A))
     #output_two_faults = np.column_stack((combo_array, B))
     #output_three_faults = np.column_stack((combo_array, C))
-    output = np.column_stack((combo_array, A, B, C))    
-    
-    
-    
-    
-    
-    
-''' 
-    # Generate combinations of drugs
-    combinations_list = []
-    total_drugs = len(drugs)
-    for n_value in range(n, -1, -1):
-        temp_list = []
-        for i in range(total_drugs):
-            for j in range(total_drugs - 1, i - 1, -1):
-                if j - i + 1 == n_value:
-                    combination = ",".join(drugs[i:j + 1])
-                    temp_list.append(combination)
-        combinations_list.extend(reversed(temp_list))      
-    print(combinations_list)
-    # Create a DataFrame for each combination
-    dfs = []
-    for i, combination in enumerate(combinations_list):
-        if i == 0:
-            combination = ('Untreated',)
-        df = pd.DataFrame({'Drugs': combination})
-        dfs.append(df)
-        #print(combination)
-        output_one_fault = np.column_stack((combinations_list, A))
-        output_two_faults = np.column_stack((combinations_list, B))
-        output_three_faults = np.column_stack((combinations_list, C))
-        #print(output_one_fault)
-        #print(output_two_faults)
-        #print(output_three_faults)
-
-    #result_df = pd.concat(dfs, ignore_index=True)
-    #print(result_df)
-'''
+    output = np.column_stack((combo_array, A, B, C))  
