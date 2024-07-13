4480bb1c5d0499c943ad99fc6688cfd486ab6d7f
PranabeshTAMU <144166072+PranabeshTAMU@users.noreply.github.com>
2024-07-13 15:31:22 -0500

    Update NSCLC_PDL1_two_faults.py

--- NSCLC_PDL1_two_faults.py
+++ NSCLC_PDL1_two_faults.py
@@ -1,5 +1,5 @@
 import numpy as np
-def NSCLC_PDL1_two_faults(fault1, fault2, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14):
+def NSCLC_PDL1_two_faults(fault1, fault2, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16):
 #inputs 
     K_Ras = 0
     EGF = 0
@@ -13,21 +13,22 @@ def NSCLC_PDL1_two_faults(fault1, fault2, x1, x2, x3, x4, x5, x6, x7, x8, x9, x1
     PAMP = 0
 
 # Drugs
-# Drugs
     Osimertinib=x1#Targets EGFR
     Selpercatinib=x2#=1 #Targets KIF5BRET
     Trastuzumab_deruxtecan=x3#Targets ERBB2
     Savolitinib=x4#Targets MET
-    Dabrafenib_Trametinib=x5#=1 #Targets RAF
+    Dabrafenib=x5#=1 #Targets RAF
     Durvalumab=x6#Targets PD_L1
-    Alpelisib=x7#=1 #Targets PI3K
-    Enzastaurin=x8#=1 #Targets PKC
-    Lumakras=x9#=1 #Targets KRAS
-    RG7388=x10#=1 #Targets MDM2
-    Ribociclib=x11#=1 #Targets CDK4/6
-    STAT_Inhibitor=x12#=1 #Targets STAT3/5
-    Everolimus=x13#=1 #Targets mTOR
-    Lorlatinib=x14#=1 #Targets EM4ALK
+    Trametinib=x7#=1 #Targets MEK
+    Alpelisib=x8#=1 #Targets PI3K
+    Enzastaurin=x9#=1 #Targets PKC
+    Lumakras=x10#=1 #Targets KRAS
+    RG7388=x11#=1 #Targets MDM2
+    Ribociclib=x12#=1 #Targets CDK4/6
+    STAT_Inhibitor=x13#=1 #Targets STAT3/5
+    Everolimus=x14#=1 #Targets mTOR
+    Lorlatinib=x15#=1 #Targets EM4ALK
+    MK2206 =x16#=1 #Targets AKT
     
     
 
@@ -170,125 +171,104 @@ def NSCLC_PDL1_two_faults(fault1, fault2, x1, x2, x3, x4, x5, x6, x7, x8, x9, x1
     if fault1 == 28 or fault2 == 28:
         MEK = 1
     else:
-        MEK = int(TRAF6 or (RAF and (not Dabrafenib_Trametinib))) 
+        MEK = int(TRAF6 or (RAF and (not Dabrafenib))) 
 
     if fault1 == 29 or fault2 == 29:
         ERK = 1
     else:
-        ERK = int(MEK) 
+        ERK = int( MEK and (not Trametinib)) 
              
     if fault1 == 30 or fault2 == 30:
-        BAD = 0
-                
-    else:
-        BAD = int(not(PKB_Akt))
-        
-    if fault1 == 31 or fault2 == 31:
-        CASP9 = 0 
-
-    else:
-        CASP9 = int(not(PKB_Akt))        
-        
-    if fault1 == 32 or fault2 == 32:         
-        Forkhead = 0
-        
-    else:
-        Forkhead = int((not(PKB_Akt)) or MST1)      
-    
-    if fault1 == 33 or fault2 == 33:        
         IKK = 1
     else:
-        IKK = int(TRIF or PKB_Akt)
+        IKK = int(TRIF or (PKB_Akt and (not MK2206)))
         
-    if fault1 == 34 or fault2 == 34:        
+    if fault1 == 31 or fault2 == 31:
         NFAT = 1
     else:
-        NFAT = int(ERK)       
+        NFAT = int(ERK)  
         
-    if fault1 == 35 or fault2 == 35:        
+    if fault1 == 32 or fault2 == 32:         
         AP1 = 1
     else:
         AP1 = int(ERK)          
-
-    if fault1 == 36 or fault2 == 36:        
+      
+    if fault1 == 33 or fault2 == 33:        
         NFKB = 1
     else:
-        NFKB = int(PKB_Akt or IKK)                  
-
-    if fault1 == 37 or fault2 == 37:        
+        NFKB = int((PKB_Akt and (not MK2206)) or IKK) 
+        
+    if fault1 == 34 or fault2 == 34:        
         PD_L1 = 1
     else:
-        PD_L1 = int(AP1 or NFAT or (STAT3by5 and (not STAT_Inhibitor)) or NFKB or DNA_Damage)                  
-
-    if fault1 == 38 or fault2 == 38:        
+        PD_L1 = int(AP1 or NFAT or (STAT3by5 and (not STAT_Inhibitor)) or NFKB or DNA)        
+        
+    if fault1 == 35 or fault2 == 35:        
         RetinoicAcid = 1
     else:
         RetinoicAcid = int(DNA)        
-        
-    if fault1 == 39 or fault2 == 39:
-        RARbeta = 1
-    else:
-        RARbeta = int(RetinoicAcid)        
-             
-    if fault1 == 40 or fault2 == 40:
+
+    if fault1 == 36 or fault2 == 36:        
         mTOR = 1
     else:
-        mTOR = int(PKB_Akt)        
+        mTOR = int(PKB_Akt and (not MK2206))                   
 
-    if fault1 == 41 or fault2 == 41:
-        S6K = 1
-    else:
-        S6K = int(mTOR and (not Everolimus))   
-
-    if fault1 == 42 or fault2 == 42:
+    if fault1 == 37 or fault2 == 37:        
         MDM2 = 1
     else:
-        MDM2 = int(DNA  or PKB_Akt)            
+        MDM2 = int(DNA  or (PKB_Akt and (not MK2206)))                   
 
-    if fault1 == 43 or fault2 == 43:
+    if fault1 == 38 or fault2 == 38:        
         P53 = 0
     else:
-        P53 = int(not(MDM2 and (not RG7388)))
+        P53 = int(not(MDM2 and (not RG7388)))       
         
-    if fault1 == 44 or fault2 == 44:        
-        P21 = 0
-    else:
-        P21 = int(P53 and P16)
-
-    if fault1 == 45 or fault2 == 45:
+    if fault1 == 39 or fault2 == 39:
         FHIT = 0
     else:
-        FHIT = int(P53) 
-    if fault1 == 46 or fault2 == 46:
+        FHIT = int(P53 and (not AP1))       
+             
+    if fault1 == 40 or fault2 == 40:
+        P21 = 0
+    else:
+        P21 = int(P53)      
+
+    if fault1 == 41 or fault2 == 41:
         CyclinD1 = 1
     else:
-        CyclinD1 = int((not RASSF1) or ERK or (STAT3by5 and (not STAT_Inhibitor)) or (not P16))
-    if fault1 == 47 or fault2 == 47:
+        CyclinD1 = int((not RASSF1) or ERK or (STAT3by5 and (not STAT_Inhibitor)) or (not P16) or (not FHIT))   
+
+    if fault1 == 42 or fault2 == 42:
         CDK4by6 = 1
     else:
         CDK4by6 = int(CyclinD1 or (not P21) or (not P16))
-    if fault1 == 48 or fault2 == 48:
-        Rb = 1
-    else:
-        Rb = int(CDK4by6 and (not Ribociclib))      
+        
+    
 
         #return NSCLC_one_fault(fault1,x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12)
 
-    Uncontrolled_Proliferation =  int(CyclinD1 or S6K or RARbeta or Rb)
-    Apoptosis =  int((not RARbeta) and P21 and FHIT and BAD and CASP9 and Forkhead)
+    
+    RARbeta =  int(RetinoicAcid)
+    CD1 = int(CyclinD1)
+    Rb = int(CDK4by6 and (not Ribociclib)) 
+    S6K =  int(mTOR and (not Everolimus))
+    BAD = int(not(PKB_Akt and (not MK2206)))
+    CASP9 = int(not(PKB_Akt and (not MK2206)))
+    Forkhead = int((not(PKB_Akt and (not MK2206))) and MST1)
+    FHIT1 = int(FHIT)
     T_Cell = int(not(PD_L1 and (not Durvalumab)))
 
 
-    output1 = [Uncontrolled_Proliferation,Apoptosis, T_Cell]
+    output1 = [RARbeta, CD1, Rb, S6K, BAD, CASP9, Forkhead, FHIT1, T_Cell]
     #print(output1)
-    output2 = [0, 1, 1]  # Ideal Output
+    output2 = [0,0,0,0,1,1,1,1,1]  # Ideal Output
 
     a = 0
     b = 0
     c = 0
     d = 0
 
-    for i in range(3):
+    for i in range(9):
         if output1[i] == 1 and output2[i] == 1:
             a += 1
         elif output1[i] == 0 and output2[i] == 1:
@@ -299,4 +279,4 @@ def NSCLC_PDL1_two_faults(fault1, fault2, x1, x2, x3, x4, x5, x6, x7, x8, x9, x1
             d += 1
 
     output = ((b + c) ** 2) / ((a + b + c + d) ** 2)
-    return(output1)
+    return(output)
